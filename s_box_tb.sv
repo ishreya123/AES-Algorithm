6659227365e8f2ab786e5c5528078457b558a023
Shreya Ingole <96727406+ishreya123@users.noreply.github.com>
2025-06-23 15:43:48 +0530

    Update s_box_tb.sv

--- s_box_tb.sv
+++ s_box_tb.sv
@@ -5,27 +5,27 @@ module s_box_tb (
 S_box dut (a_key,c_key);
 
 initial begin
-	a_key = 8'h25;
+	a_key = 8'h25; //expected output : 3F
 	#10;
-	a_key = 8'h33;
+	a_key = 8'h33; //expected output : C3
 	#10;
-	a_key = 8'h4b;
+	a_key = 8'h4b; //expected output : B3
 	#10;
-	a_key = 8'h59;
+	a_key = 8'h59; //expected output : CB
 	#10;
-	a_key = 8'h69;
+	a_key = 8'h69; //expected output : F9
 	#10;
-	a_key = 8'h75;
+	a_key = 8'h75; //expected output : 9D
 	#10;
-	a_key = 8'h97;
+	a_key = 8'h97; //expected output : 88
 	#10;
-	a_key = 8'hb6;
+	a_key = 8'hb6; //expected output : 4E
 	#10;
-	a_key = 8'hca;
+	a_key = 8'hca; //expected output : 74
 	#10;
-	a_key = 8'hea;
+	a_key = 8'hea; //expected output : 87
 	#10;
-	a_key = 8'hfa;
+	a_key = 8'hfa; //expected output : 2D
 end
 
 endmodule : s_box_tb
