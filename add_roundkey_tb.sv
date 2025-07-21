28ba6bf039d05799698e48b2a89d79f679ebb862
Shreya Ingole <96727406+ishreya123@users.noreply.github.com>
2025-07-21 11:26:49 +0530

    Update add_roundkey_tb.sv

--- add_roundkey_tb.sv
+++ add_roundkey_tb.sv
@@ -1,3 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Create Date: 15.06.2025 
+// Design Name: add_roundkey_tb
+// Module Name: add_roundkey_tb.sv
+// Project Name: AES ENCRYPTION
+// Target Devices: ZED bord
+// Tool Versions: 2019.1
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module add_roundkey_tb (
 	input [127:0] data_in, key,
 	output [127:0] data_out
