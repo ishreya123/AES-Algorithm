b33103e9d3fee67facfd774e395f584f00c5367b
Neeraj Chhajed <100212332+NEERAJCHHAJED@users.noreply.github.com>
2025-07-23 12:01:01 +0530

    Update add_roundkey_tb.sv

--- add_roundkey_tb.sv
+++ add_roundkey_tb.sv
@@ -23,7 +23,7 @@ module add_roundkey_tb (
 );
 AddRoundKey dut (data_in, data_out, key);
 
-intial begin
+initial begin
 	data_in = 128'h3243f6a8885a308d313198a2e0370734;
 	key = 128'h2b7e151628aed2a6abf7158809cf4f3c;
 
