b8d4465f25ac178bfb47502208ec84a968a9ae8d
Dipti Khairkar <dkhairkar@gmail.com>
2025-07-23 19:37:01 +0530

    Update Aes_top.sv

--- Aes_top.sv
+++ Aes_top.sv
@@ -15,6 +15,7 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
+// Revision 1.0 - Final version
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
@@ -25,43 +26,48 @@ input[127:0] in;
 //input[N-1:0] key;
 input[127:0] key;
 output [127:0] out;
-//wire [(128*(Nr+1))-1:0] fullkeys;
-wire [1407:0] fullkeys;
-wire [127:0] states [11:0];
+
+parameter Nr=10;
+parameter Nk=4;
+
+wire [(128*(Nr+1))-1:0] fullkeys;
+//wire [1407:0] fullkeys;
+wire [127:0] states [Nr+1:0];
 wire [127:0] after_subBytes;
 wire [127:0] after_shiftRows;
 
 //key_expansion #(Nr,N,Nk) ke (Nr,key,fullkeys);
-key_expansion ke (4'b1010,key,fullkeys);
+key_expansion #(Nk, Nr) ke (key,fullkeys);
 //num_round, input_key, output_key
-//AddRoundKey addrk1(in, fullkeys[((128*(Nr+1))-1)-:128],states[0]);
-AddRoundKey addrk1(in, fullkeys[1407-:128],states[0]);
+AddRoundKey addrk1(in, fullkeys[128*Nr+:128],states[0]);
+//AddRoundKey addrk1(in, fullkeys[1407-:128],states[0]);
+
 genvar i;
 
 generate
 
-    //for(i=1;i<Nr;i=i+1)begin:loop
-    for(i=1;i<10;i=i+1)begin:loop
-        //encrypt_round er(states[i-1],fullkeys[(((128*(Nr+1))-1)-128*i)-:128],states[i]);
-        encrypt_round er(states[i-1],fullkeys[(1407-128*i)-:128],states[i]);
-        
+    for(i=1;i<Nr;i=i+1)begin:loop
+    //for(i=1;i<10;i=i+1)begin:loop
+        encrypt_round er(states[i-1],fullkeys[(((128*(Nr+1))-1)-128*i)-:128],states[i]);
+        //encrypt_round er(states[i-1],fullkeys[(1407-(128*i))-:128],states[i]);
         end
-        SubBytes sub(in, after_subBytes);
+        
+        SubBytes sub(states[Nr-1], after_subBytes);
         Shift_rows shift(after_subBytes,after_shiftRows);
         //AddRoundKey ark(after_shiftRows,fullkeys[127:0],states[Nr]);
-        AddRoundKey ark(after_shiftRows,fullkeys[127:0],states[10]);
-        assign out = states[10];
+        AddRoundKey ark(after_shiftRows,fullkeys[127:0],states[Nr]);
+        
+        assign out = states[Nr];
         
 endgenerate
-integer j;
-always @* begin
+
+
+initial begin
 #10;
-$display("fullkeys: %h",fullkeys);
-$display("Shift_rows: %h",after_shiftRows);
-for (j = 0; j <= 10; j = j + 1) begin
-        $display("states[%0d]: %h", j, states[j]);
-    end
-$display("AddRoundKey: %h",out);
+$monitor ("Round keys (full keys)= %h",fullkeys);
+$monitor ("Subbytes keys (sub bytes)= %h",after_subBytes);
+$monitor ("After Shiftrows = %h", after_shiftRows);
+$monitor ("Final output = %h", out);
 end
 
 endmodule
