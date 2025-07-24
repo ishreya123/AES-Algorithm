e86301e33396a901549a3e7040d6709603cc9e1e
Dipti Khairkar <dkhairkar@gmail.com>
2025-07-24 19:23:06 +0530

    Create aes_transaction.sv

---
+++ Verification Environment/aes_transaction.sv
@@ -0,0 +1,7 @@
+class aes_transaction extends uvm_sequence_item;
+  `uvm_object_utils(aes_transaction)
+
+  function new(string name = "aes_transaction");
+        super.new(name);
+    endfunction
+endclass
