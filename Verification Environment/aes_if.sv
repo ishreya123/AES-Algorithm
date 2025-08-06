29f2ab0c8f43f933addf3859f55437a2d9e5bb2e
Dipti Khairkar <dkhairkar@gmail.com>
2025-08-06 13:04:33 +0530

    Update aes_if.sv

--- Verification Environment/aes_if.sv
+++ Verification Environment/aes_if.sv
@@ -1,1 +1,12 @@
+interface aes_if;
+  logic clk;
+  logic reset;
+  logic start_encrypt;
+  logic [127:0] plaintext;
+  logic [127:0] key;
+  logic [127:0] ciphertext;
+  logic encrypt_done;
+  // Add an event for driver-monitor synchronization
+  event driver_done_event;
 
+endinterface
