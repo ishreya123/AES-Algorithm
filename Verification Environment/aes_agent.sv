9168795e6cabc8251ab1a8da35b87af83b5e9716
Dipti Khairkar <dkhairkar@gmail.com>
2025-08-06 12:58:46 +0530

    Update aes_agent.sv

--- Verification Environment/aes_agent.sv
+++ Verification Environment/aes_agent.sv
@@ -1,24 +1,26 @@
 class aes_agent extends uvm_agent;
   `uvm_component_utils(aes_agent)
-  
-    aes_driver drv;
-    aes_monitor mon;
-  uvm_sequencer #(aes_transaction) seqr;
 
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction
+  aes_driver driver;
+  aes_monitor monitor;
+  uvm_sequencer#(aes_transaction) sequencer;
 
-    virtual function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        `uvm_info("AES_AGENT","Entered build phase",UVM_NONE);
-        drv = aes_driver::type_id::create("drv", this);
-        mon = aes_monitor::type_id::create("mon", this);
-      seqr = uvm_sequencer#(aes_transaction)::type_id::create("seqr", this);
-    endfunction
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
 
-    virtual function void connect_phase(uvm_phase phase);
-        drv.seq_item_port.connect(seqr.seq_item_export);
-      `uvm_info("AES_AGENT","Connection between driver and sequencer is done",UVM_NONE);
-    endfunction
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    `uvm_info("AES_AGENT", "Entered build phase", UVM_LOW);
+    driver = aes_driver::type_id::create("driver", this);
+    monitor = aes_monitor::type_id::create("monitor", this);
+    sequencer = uvm_sequencer#(aes_transaction)::type_id::create("sequencer", this);
+    `uvm_info("AES_AGENT", "Entered build phase", UVM_LOW);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);
+    `uvm_info("AES_AGENT", "Driver port and sequencer connection is done ", UVM_LOW);
+  endfunction
+
 endclass
