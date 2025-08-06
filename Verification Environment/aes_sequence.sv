959c74afd4604951a7516a12cc387b9b6ebdfa24
Dipti Khairkar <dkhairkar@gmail.com>
2025-08-06 13:01:58 +0530

    Update aes_sequence.sv

--- Verification Environment/aes_sequence.sv
+++ Verification Environment/aes_sequence.sv
@@ -1,9 +1,22 @@
-class aes_sequence extends uvm_sequence #(aes_transaction);
+class aes_sequence extends uvm_sequence#(aes_transaction);
   `uvm_object_utils(aes_sequence)
 
   function new(string name = "aes_sequence");
-        super.new(name);
-    endfunction
+    super.new(name);
+  endfunction
 
+  task body();
+    `uvm_info("AES_SEQUENCE", "Starting aes_base_sequence...", UVM_LOW)
+    repeat(1) begin
+      aes_transaction seq_item = aes_transaction::type_id::create("seq_item");
+      start_item(seq_item);
+      seq_item.plaintext = 128'h3243f6a8885a308d313198a2e0370734;
+      seq_item.key       = 128'h2b7e151628aed2a6abf7158809cf4f3c;
+     // Set reference value!
+      `uvm_warning("SEQ_ITEM_CHECK",$sformatf("PLAINTEXT: %h | KEY: %h ", seq_item.plaintext, seq_item.key));
+      finish_item(seq_item);
+    end
+    `uvm_info("AES_SEQUENCE", "Finished aes_base_sequence.", UVM_LOW)
+  endtask
 
 endclass
