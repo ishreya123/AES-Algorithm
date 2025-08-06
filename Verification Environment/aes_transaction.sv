c1c2f62e991b81b9d779cd2c21180d9a419ceb16
Dipti Khairkar <dkhairkar@gmail.com>
2025-08-06 13:03:17 +0530

    Update aes_transaction.sv

--- Verification Environment/aes_transaction.sv
+++ Verification Environment/aes_transaction.sv
@@ -1,7 +1,17 @@
 class aes_transaction extends uvm_sequence_item;
-  `uvm_object_utils(aes_transaction)
+  
+  bit [127:0] plaintext;
+  bit [127:0] key;
+  bit [127:0] ciphertext;
+  
+  `uvm_object_utils_begin(aes_transaction)
+    `uvm_field_int(plaintext, UVM_ALL_ON)
+    `uvm_field_int(key, UVM_ALL_ON)
+    `uvm_field_int(ciphertext, UVM_ALL_ON)
+  `uvm_object_utils_end
 
   function new(string name = "aes_transaction");
-        super.new(name);
-    endfunction
+    super.new(name);
+  endfunction
+  
 endclass
