a51440500ecbcdba7b7b4c1e974c0a4ed605d50f
Dipti Khairkar <dkhairkar@gmail.com>
2025-08-06 12:57:35 +0530

    Update aes_test.sv

--- Verification Environment/aes_test.sv
+++ Verification Environment/aes_test.sv
@@ -1,21 +1,28 @@
 class aes_test extends uvm_test;
   `uvm_component_utils(aes_test)
-  
-    aes_env env;
-    aes_sequence seq;
-  
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction
 
-    virtual function void build_phase(uvm_phase phase);
-      `uvm_info("AES_TEST","Entered build phase",UVM_NONE);
-        super.build_phase(phase);
-        env = aes_env::type_id::create("env", this);
-    endfunction
+  aes_env env;
+  aes_sequence seq;
 
-    function void end_of_elaboration_phase(uvm_phase phase);
-    	uvm_top.print_topology();
-    endfunction
-  
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    `uvm_info("AES_TEST", "Entered build phase", UVM_LOW);
+    env = aes_env::type_id::create("env", this);
+  endfunction
+
+  function void end_of_elaboration_phase(uvm_phase phase);
+    uvm_top.print_topology();
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq = aes_sequence::type_id::create("seq");
+    seq.start(env.agent.sequencer);
+    phase.drop_objection(this);
+  endtask
+
 endclass
