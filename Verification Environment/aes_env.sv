bfc4ec79361b2a0434f02988061bc5f3edbc1f46
Dipti Khairkar <dkhairkar@gmail.com>
2025-08-06 12:58:14 +0530

    Update aes_env.sv

--- Verification Environment/aes_env.sv
+++ Verification Environment/aes_env.sv
@@ -1,21 +1,22 @@
 class aes_env extends uvm_env;
   `uvm_component_utils(aes_env)
-    aes_agent agent;
-    aes_scoreboard sb;
 
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction
+  aes_agent agent;
+  aes_scoreboard scoreboard;
 
-    virtual function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-      	`uvm_info("AES_ENV","Entered build phase",UVM_NONE);
-        agent = aes_agent::type_id::create("agent", this);
-        sb = aes_scoreboard::type_id::create("sb", this);
-    endfunction
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
 
-    virtual function void connect_phase(uvm_phase phase);
-      agent.mon.ap.connect(sb.monitor_imp);
-        `uvm_info("AES_ENV","Connection between monitor and scoreboard is done",UVM_NONE);
-    endfunction
+  function void build_phase(uvm_phase phase);
+    `uvm_info("AES_ENV", "Entered build phase", UVM_LOW);
+    agent = aes_agent::type_id::create("agent", this);
+    scoreboard = aes_scoreboard::type_id::create("scoreboard", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    agent.monitor.mon_ap.connect(scoreboard.item_export);
+    `uvm_info("AES_ENV", "Monitor to scoreboard connection done", UVM_NONE);
+  endfunction
+
 endclass
